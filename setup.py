70ca068f4e1eb9a34a92dfd4b4ce3b38178bba71
qingyiw <sunnywang950529@hotmail.com>
2021-06-23 13:44:38 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 23 16:06:56 2021
+
+@author: qingyi
+"""
+
+survey_dir = "C:/Users/wangqi44/Dropbox (MIT)/DOE_TSMS/00_Data/raw_data/travel_survey/Chicago_DailyTravel/MyDailyTravelData/"
+data_dir = "C:/Users/wangqi44/Dropbox (MIT)/project_image_demand/data/"
