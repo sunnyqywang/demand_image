5b94fa0222ea9de7d288274723001cf4b08a5a76
qingyi <sunnywang950529@hotmail.com>
2022-01-21 01:29:19 -0500

    fix mnl, add model PMA_2 where demo is input

--- models/mnl.py
+++ models/mnl.py
@@ -7,7 +7,7 @@ class MNL(nn.Module):
         super(MNL, self).__init__()
         # The first alternative is the reference
 #         self.beta = nn.Linear(n_features, n_alts-1) 
-        self.beta = nn.Linear(n_features, n_alts) 
+        self.beta = nn.Linear(n_features, n_alts)
 
         if n_alt_specific > 0:
             self.beta_ref = nn.Parameter(torch.rand(n_alt_specific))
@@ -31,3 +31,43 @@ class MNL(nn.Module):
         return V #F.relu(V)
 
 
+
+class MNL2(nn.Module):
+    def __init__(self, n_alts, dim_embed, dim_demo, dim_hidden=128):
+        super(MNL2, self).__init__()
+        self.dim_embed = dim_embed
+        self.dim_demo = dim_demo
+        
+#         self.bn_e = nn.BatchNorm1d(dim_embed)
+        self.bn_d = nn.BatchNorm1d(dim_demo)
+#         self.bn = nn.BatchNorm1d(dim_embed+dim_demo)
+    
+        self.fc_embedding1 = nn.Linear(dim_embed, dim_hidden)
+        self.fc_embedding2 = nn.Linear(dim_hidden, dim_demo)
+
+        self.fc_demo1 = nn.Linear(dim_demo, dim_demo)
+        self.fc_demo2 = nn.Linear(dim_demo, dim_demo)
+
+        self.mnl = nn.Linear(2*dim_demo, n_alts)
+        
+    def forward(self, x):
+        
+        assert x.shape[1] == self.dim_embed + self.dim_demo
+#         x = self.bn(x)
+        
+        demo = x[:, :self.dim_demo]
+        embedding = x[:, self.dim_demo:]
+        
+        demo = self.bn_d(demo)
+#         embedding = self.bn_e(embedding)
+        
+        embedding = F.relu(self.fc_embedding1(embedding))
+        embedding = self.fc_embedding2(embedding)
+        
+        demo = F.relu(self.fc_demo1(demo))
+        demo = self.fc_demo2(demo)
+        
+        V = self.mnl(torch.concat([demo, embedding], dim=1))
+        
+        return V
+    
