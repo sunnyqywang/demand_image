dca47bdc1e7edbda72b81c05f9e5366611a3ff19
qingyi <sunnywang950529@hotmail.com>
2022-02-08 16:48:05 -0500

    change direct cv regression to tract based

--- models/direct_regression.py
+++ models/direct_regression.py
@@ -8,14 +8,27 @@ class DirectRegression(nn.Module):
         super(DirectRegression, self).__init__()
         
         self.encoder = config['encoder']
-        self.fc1 = nn.Linear(2048*config['model_config']['output_dim']**2, 1)
-        self.fc2 = nn.Linear(512, 128)
-        self.fc3 = nn.Linear(128, 1)
+        self.encoder_output_dim = 2048*config['model_config']['output_dim']**2
+#         self.fc1 = nn.Linear(self.encoder_output_dim, self.encoder_output_dim//4)
+#         self.fc2 = nn.Linear(self.encoder_output_dim//4, self.encoder_output_dim//16)
+#         self.fc3 = nn.Linear(self.encoder_output_dim//16, config['model_config']['n_alts'])
+        self.fc = nn.Linear(self.encoder_output_dim, config['model_config']['n_alts'])
+        self.bn = nn.BatchNorm1d(self.encoder_output_dim)
 
+#         self.dropout = nn.Dropout(p=config['model_config']['dropout'])
+        
     def forward(self, x):
-        xp = self.encoder(x) 
-#         xd = torch.sigmoid(self.fc3(F.relu(self.fc2(F.relu(self.fc1(xp.view(len(xp), -1)))))))
-#         xd = self.fc3(F.relu(self.fc2(F.relu(self.fc1(xp.view(len(xp), -1))))))
-        xd = self.fc1(xp.view(len(xp), -1))
+        batch_size,num_im, _,_,_ = x.shape
+                
+        for i in range(num_im):
+            if i == 0:
+                xp = self.encoder(x[:,i,:,:,:]).view(batch_size, -1)
+            else:
+                xp += self.encoder(x[:,i,:,:,:]).view(batch_size, -1)
+
+        xp /= len(x)
+        xp = self.bn(xp)
+#         xp = self.fc3(self.dropout(F.relu(self.fc2(self.dropout(F.relu(self.fc1(xp)))))))
+        xp = self.fc(xp)
     
-        return xd
+        return xp
