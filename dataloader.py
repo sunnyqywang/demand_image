0412-c1 architecture, fix demo vars normalization

dataloader.py
@@ -282,8 +282,9 @@ def load_demo_v2(data_dir):
     # Smart Locations from EPA
     epa = pd.read_csv(data_dir+"EPA_SmartLocations_CensusTract_export.csv")
     # log_tranform
-    epa['activity_density'] = np.log(epa['activity_density'])
-    
+#     epa['activity_density'] = np.log(epa['activity_density'])
+#     epa['auto_oriented'] = np.log(epa['auto_oriented'])
+
     # Pollution PM2.5
     pm = pd.read_csv(data_dir+"PM2.5_Concentrations_2016_Illinois_Average_Annual.csv")
     pm['ctfips'] = pm['ctfips'] % 1e6
@@ -293,26 +294,32 @@ def load_demo_v2(data_dir):
     # ACS Demo
     acs = pd.read_csv(data_dir+"demo_tract.csv")
     acs['pop_density'] = acs['tot_population'] / acs['area']
+#     acs['pop_density'] = np.log(acs['pop_density'])
     demo_df = pd.merge(demo_df, acs, left_on=['COUNTYFP','TRACTCE'], right_on=['COUNTYA','TRACTA'])
 
     # normalize
     real_columns = ['activity_density','auto_oriented','multi_modal','pedestrian_oriented','PM2.5']
     real_columns += ['pop_density','inc_per_capita']
 
+#     return demo_df, None
+
     for c in real_columns:
         # demo_df[c] = (demo_df[c] - demo_df[c].mean())/demo_df[c].std()
-        demo_df[c] = demo_df[c]/demo_df[c].max()
+        demo_df[c] = (demo_df[c]-demo_df[c].min())/(demo_df[c].max()-demo_df[c].min())
         demo_df[c] = (demo_df[c]-0.5)/0.5
 
     pct_columns = ['employment_entropy','pop_income_entropy','wrk_emp_balance']
     pct_columns += ['pct25_34yrs','pct35_50yrs','pctover65yrs',
                  'pctwhite_alone','pct_nonwhite']
     for c in pct_columns:
+        demo_df[c] = (demo_df[c]-demo_df[c].min())/(demo_df[c].max()-demo_df[c].min())
         demo_df[c] = (demo_df[c] - 0.5)/0.5
 
-    columns = ['pop_density','inc_per_capita','pct25_34yrs','pct35_50yrs','pctover65yrs',
-                 'pctwhite_alone','pct_nonwhite']
-
+#     columns = ['pop_density','inc_per_capita','pct25_34yrs','pct35_50yrs','pctover65yrs',
+#                  'pctwhite_alone','pct_nonwhite']
+    columns = ['activity_density','multi_modal','pedestrian_oriented','PM2.5',
+               'inc_per_capita','employment_entropy','wrk_emp_balance','pct25_34yrs']
+    
     return demo_df, columns
 
 def load_demo_v1(data_dir, norm=2):

models/stylegan2/csgan2.py
@@ -126,7 +126,7 @@ class Generator(nn.Module):
         x = self.initial_conv(x)
 
         for style, block, attn in zip(styles, self.blocks, self.attns):
-            style = style if self.condition_on_mapper else torch.cat((style, conditions), 1).float()
+#             style = style if self.condition_on_mapper else torch.cat((style, conditions), 1).float()
             if exists(attn):
                 x = attn(x)
             x, rgb = block(x, rgb, style, input_noise)
@@ -174,7 +174,10 @@ class Discriminator(nn.Module):
 
         self.final_conv = nn.Conv2d(chan_last, chan_last, 3, padding=1)
         self.flatten = Flatten()
-        self.to_logit = nn.Linear(latent_dim + condition_dim, 1)
+        
+        self.phi = nn.Linear(latent_dim, condition_dim)
+        self.psix = nn.Linear(condition_dim, 1)
+        self.psiy = nn.Linear(condition_dim, 1)
 
     def forward(self, x, conditions):
         b, *_ = x.shape
@@ -193,9 +196,15 @@ class Discriminator(nn.Module):
 
         x = self.final_conv(x)
         x = self.flatten(x)
-        x = torch.cat([x, conditions], dim=1).float()
-        x = self.to_logit(x)
-        return x.squeeze(), quantize_loss
+        x = self.phi(x)
+        
+        out = self.psix(x) + self.psiy(x*conditions)
+        
+        return out, None
+#         x = torch.cat([x, conditions], dim=1).float()
+#         x = self.to_logit(x)
+#         return x.squeeze(), quantize_loss
+#         return x, self.to_logit(x).squeeze()
 
     
 class StyleGAN2(nn.Module):

models/stylegan2/csgan2_trainer.py
@@ -95,7 +95,6 @@ class Trainer():
         is_ddp = False,
         rank = 0,
         world_size = 1,
-        log = False,
         *args,
         **kwargs
     ):
@@ -190,8 +189,8 @@ class Trainer():
         self.rank = rank
         self.world_size = world_size
 
-        self.logger = aim.Session(experiment=name) if log else None
-
+        self.logger = {}
+        
     @property
     def image_extension(self):
         return 'jpg' if not self.transparent else 'png'
@@ -215,10 +214,8 @@ class Trainer():
             self.D_ddp = DDP(self.GAN.D, **ddp_kwargs)
             self.D_aug_ddp = DDP(self.GAN.D_aug, **ddp_kwargs)
 
-        if exists(self.logger):
-            self.logger.set_params(self.hparams)
-
     def write_config(self):
+        torch.save(self.dataset.demo_df, self.models_dir / 'demo_df.pt')
         self.config_path.write_text(json.dumps(self.config()))
 
     def load_config(self):
@@ -253,7 +250,7 @@ class Trainer():
             self.aug_prob = min(0.5, (1e5 - num_samples) * 3e-6)
             print(f'autosetting augmentation probability to {round(self.aug_prob * 100)}%')
 
-    def train(self):
+    def train(self, log=True):
         assert exists(self.loader), 'You must first initialize the data source with `.set_data_src(<folder of images>)`'
 
         if not exists(self.GAN):
@@ -331,8 +328,8 @@ class Trainer():
             # Get real images and demos
 
             _,image_batch,demo_batch = next(self.loader)
-            image_batch = torch.flatten(image_batch, start_dim=0, end_dim=-4)
-            demo_batch = torch.flatten(demo_batch, start_dim=0, end_dim=-2)
+            image_batch = torch.flatten(image_batch, start_dim=0, end_dim=-4).float()
+            demo_batch = torch.flatten(demo_batch, start_dim=0, end_dim=-2).float()
             demo_batch = demo_batch.cuda(self.rank)         
             image_batch = image_batch.cuda(self.rank)
             image_batch.requires_grad_()
@@ -378,7 +375,8 @@ class Trainer():
             if apply_gradient_penalty:
                 gp = gradient_penalty(image_batch, real_output)
                 self.last_gp_loss = gp.clone().detach().item()
-                self.track(self.last_gp_loss, 'GP')
+                if log:
+                    self.track(self.last_gp_loss, 'GP')
                 disc_loss = disc_loss + gp
 
             disc_loss = disc_loss / self.gradient_accumulate_every
@@ -388,7 +386,8 @@ class Trainer():
             total_disc_loss += divergence.detach().item() / self.gradient_accumulate_every
 
         self.d_loss = float(total_disc_loss)
-        self.track(self.d_loss, 'D')
+        if log:
+            self.track(self.d_loss, 'D')
 
         self.GAN.D_opt.step()
 
@@ -399,8 +398,8 @@ class Trainer():
         for i in gradient_accumulate_contexts(self.gradient_accumulate_every, self.is_ddp, ddps=[S, G, D_aug]):
             
             _,image_batch,demo_batch = next(self.loader)
-            image_batch = torch.flatten(image_batch, start_dim=0, end_dim=-4)
-            demo_batch = torch.flatten(demo_batch, start_dim=0, end_dim=-2)
+            image_batch = torch.flatten(image_batch, start_dim=0, end_dim=-4).float()
+            demo_batch = torch.flatten(demo_batch, start_dim=0, end_dim=-2).float()
             demo_batch = demo_batch.cuda(self.rank)         
             image_batch = image_batch.cuda(self.rank)
             image_batch.requires_grad_()
@@ -410,7 +409,7 @@ class Trainer():
             w_space = latent_to_w(S, style, demo_batch)           
             w_styles = styles_def_to_tensor(w_space)
             noise = image_noise(batch_size, image_size, device=self.rank)
-
+            
             generated_images = G(w_styles, noise, demo_batch)
 #             fake_images_to_disc = torch.cat([generated_images.clone().detach(), demo_conv], 1).float()
             fake_images_to_disc = generated_images
@@ -452,13 +451,15 @@ class Trainer():
             total_gen_loss += loss.detach().item() / self.gradient_accumulate_every
 
         self.g_loss = float(total_gen_loss)
-        self.track(self.g_loss, 'G')
+        
+        if log:
+            self.track(self.g_loss, 'G')
 
         self.GAN.G_opt.step()
 
         # calculate moving averages
 
-        if apply_path_penalty and not np.isnan(avg_pl_length):
+        if apply_path_penalty and not np.isnan(avg_pl_length) and log:
             self.pl_mean = self.pl_length_ma.update_average(self.pl_mean, avg_pl_length)
             self.track(self.pl_mean, 'PL')
 
@@ -506,8 +507,9 @@ class Trainer():
         num_layers = self.GAN.G.num_layers
 
         # latents and noise
-        noise_demo = self.dataset.demo_df[:num_rows,:]        
-        demo_batch = noise_demo.repeat_interleave(num_rows, dim=0).cuda(self.rank)
+#         noise_demo = self.dataset.demo_df[:num_rows,:]        
+        noise_demo = self.dataset.demo_df[[1095, 1093, 299, 491, 626, 1184, 1328, 311]]
+        demo_batch = noise_demo.repeat_interleave(num_rows, dim=0).float().cuda(self.rank)
         latents = noise_list(num_rows ** 2, num_layers, latent_dim, device=self.rank)       
         n = image_noise(num_rows ** 2, image_size, device=self.rank)
 
@@ -515,8 +517,10 @@ class Trainer():
         generated_images = self.generate_truncated(self.GAN.S, self.GAN.G, latents, n, demo_batch, trunc_psi = self.trunc_psi)
         
         torchvision.utils.save_image(generated_images, str(self.results_dir / self.name / f'{str(num)}.{ext}'), nrow=num_rows)
-            
-        return demo_batch
+        
+        fake_output, fake_q_loss = self.GAN.D_aug(generated_images, conditions = demo_batch, detach = True)
+
+        return fake_output
         # moving averages
 #         generated_images = self.generate_truncated(self.GAN.SE, self.GAN.GE, latents, n, demo_batch, trunc_psi = self.trunc_psi)
 #         torchvision.utils.save_image(generated_images, str(self.results_dir / self.name / f'{str(num)}-ema.{ext}'), nrow=num_rows)
@@ -732,9 +736,10 @@ class Trainer():
         print(log)
 
     def track(self, value, name):
-        if not exists(self.logger):
-            return
-        self.logger.track(value, name = name)
+        if name not in self.logger.keys():
+            self.logger[name] = [value]
+        else:
+            self.logger[name].append(value)
 
     def model_name(self, num=None):
         if num is None:

models/stylegan2/util.py
@@ -315,7 +315,7 @@ class StyleVectorizer(nn.Module): ## mapping function z->w
 
         self.condition_on_mapper = condition_on_mapper
         if condition_on_mapper:
-            emb_in = emb+condition_dim
+            emb_in = condition_dim
         else:
             emb_in = emb
             
@@ -327,7 +327,8 @@ class StyleVectorizer(nn.Module): ## mapping function z->w
 
     def forward(self, x, labels=None):
         if self.condition_on_mapper:
-            x = torch.cat([x, labels], dim=1).float()
+            x = labels
+#             x = torch.cat([x, labels], dim=1).float()
             
         x = F.normalize(x, dim=1)
         x = self.net(x)